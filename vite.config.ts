4a25665a780275afc77636ed30edb9266f4adaa7
Rishaan <codeandro@gmail.com>
2026-05-01 12:14:58 +1000

    cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,11 @@ import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, '.', '');
+  
   return {
     plugins: [react(), tailwindcss()],
-	base: '/nehanint.github.io/',
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    },
+	base: '/',
+   
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
